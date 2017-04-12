eefe39e9bf25fe52178188431b70fc9fbdffa07a
José Malcher Junior <contato@josemalcher.net>
2017-04-12 16:04:36 -0300

    Aula 06 Mod 2: HTML DOM - Propriedades

---
+++ 02-Models/js/htmlDomPropriedades.js
@@ -0,0 +1,13 @@
+/**
+ * Created by josemalcher on 12/04/2017.
+ */
+//Aula Mod 2 Aula 6 - HTML DOM Propriedades
+//https://www.w3schools.com/js/js_htmldom_document.asp
+
+window.onload = function () {
+    //document.forms.namedItem("nomeDoFurmulario").style.display = "none";
+    //alert(document.forms.namedItem("nomeDoFurmulario"));
+
+    alert(document.forms.length);
+
+}
